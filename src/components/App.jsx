e81787166e5f31d1c35790b27657491e88ce6291
Kooding <giyun529@hanmail.net>
2021-01-12 09:27:16 +0900

    useEffect remove

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,8 @@
-import React, { useEffect } from "react";
-import Cookies from "js-cookie";
+import React from "react";
 import "../App.css";
 import Routes from "./Routes.jsx";
 
-function App({ history }) {
-  useEffect(() => {
-    const IsCookie = Cookies.get("x_auth");
-    if (IsCookie !== undefined) {
-      console.log("토큰확인완료");
-      //수정할부분..
-      history.push("/home");
-    }
-  }, [history]);
-
+function App() {
   return (
     <div className="App">
       <Routes />
