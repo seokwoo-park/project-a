2c1b289738c50bc2fd77976a0aa2cf747e1eb9df
Kooding <giyun529@hanmail.net>
2021-01-12 09:32:31 +0900

    redirction by cookie

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -3,11 +3,19 @@ import Auth from "../routes/Auth.jsx";
 import Login from "../routes/LogIn.jsx";
 import SignUp from "../routes/SignUp.jsx";
 import Home from "../routes/Home.jsx";
-import { Route, Switch } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 function Routes() {
+  const [cookies] = useCookies(["x_auth"]);
+
   return (
     <div>
+      {cookies.x_auth !== undefined ? (
+        <Redirect to="/home" />
+      ) : (
+        <Redirect to="/" />
+      )}
       <Switch>
         <Route exact path="/" component={Auth} />
         <Route path="/home" component={Home} />
