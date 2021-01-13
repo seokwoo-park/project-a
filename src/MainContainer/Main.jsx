e99583ab4d368285d48b0d17a348c7d9a5018e43
seokwoo-park <p.seokwoo@gmail.com>
2021-01-13 19:49:30 +0900

    Post Upload edited to Modal

--- src/MainContainer/Main.jsx
+++ src/MainContainer/Main.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
 import PostUpload from "./components/PostUpload";
+import { Button } from 'react-bootstrap';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import './css/Main.css';
+
+
+
+
+
 
 
 function Main({ history }) {
 
+
+  const [postModalShow,setPostModalShow] = useState(false);
+
   return (
     <div className="main">
-      <PostUpload/>
+
+      {postModalShow? 
+      <PostUpload postModalShow={postModalShow} setPostModalShow={setPostModalShow}/>
+       : null}
+
+      <Button className="main_post_btn" onClick={()=>{setPostModalShow(true)}}>
+        Post <PostAddIcon/> 
+      </Button>
+
+
+
     </div>
   );
 }
--- src/MainContainer/components/PostUpload.jsx
+++ src/MainContainer/components/PostUpload.jsx
@@ -1,14 +1,16 @@
 import React ,{useState} from 'react'
 import '../css/PostUpload.css';
+import {Button,Modal} from 'react-bootstrap';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 
-function PostUpload() {
+function PostUpload({setPostModalShow,postModalShow}) {
 
     const [postInput,setPostInput] = useState('');
-
+    
     const [imageView, setImageView] = useState('');
     const [imgStyle, setImgStyle] = useState({});
 
-    
+    const handleClose = () => setPostModalShow(false);    
 
     function onSubmitHandler(e) {
         e.preventDefault();
@@ -28,32 +30,54 @@ function PostUpload() {
 
 
     return (
+
         <div className="postUpload">
-            <form className="postUpload_form" onSubmit={onSubmitHandler}>
-                <img className="img_preview" src={imageView} style={imgStyle}/>
+            <Modal
+                show={postModalShow}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Share your days</Modal.Title>
+                </Modal.Header>
 
-                <textarea required
-                 className="postUpload_text" 
-                 maxLength={200}
-                 onChange={(e)=>{setPostInput(e.target.value)}}
-                 type="text"
-                 rows={5} 
-                 cols={50}
-                 value={postInput} 
-                 placeholder="What's on your mind ?"/>
+                <Modal.Body>
+                    <form className="postUpload_form" onSubmit={onSubmitHandler}>
+                        <textarea required
+                        className="postUpload_text" 
+                        maxLength={200}
+                        onChange={(e)=>{setPostInput(e.target.value)}}
+                        type="text"
+                        rows={5} 
+                        cols={50}
+                        value={postInput} 
+                        placeholder="What's on your mind ?"/>
+                    </form>
 
+                    <div className="postUpload_form_files">
+                        <label htmlFor="files"> <AddAPhotoIcon/>Photo </label>
+                        <input id="files"
+                        onChange={onFileChange} 
+                        type="file" 
+                        accept="image/*" 
+                        style={{display:"none"}}/>
 
-                <button onSubmit={onSubmitHandler} type="submit">Post</button>
-            </form>
-            <div className="postUpload_form_files">
-                <label for="files">➕Photo </label>
-                <input id="files"
-                onChange={onFileChange} 
-                type="file" 
-                accept="image/*" 
-                style={{display:"none"}}/>
-            </div>
+                        <img className="img_preview" src={imageView} style={imgStyle}/>
+                    </div>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                    Close
+                    </Button>
+                    <Button variant="primary" onClick={onSubmitHandler} type="submit">Submit</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
+
+
+        
     )
 }
 
