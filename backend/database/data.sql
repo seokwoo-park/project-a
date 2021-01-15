3a78198a91fee681e04cf351810d0603b629bed4
seokwoo-park <p.seokwoo@gmail.com>
2021-01-15 20:26:54 +0900

    removed /backend in gitignore

---
+++ backend/database/data.sql
@@ -0,0 +1,25 @@
+/****배포시 삭제해야합니다. 참고용
+
+//유저테이블
+
+create table `side_project_user`(
+    `idx` int(11) not null auto_increment,
+    `user_id` varchar(300) not null unique,
+    `user_pw` varchar(1024) not null,
+    `email` varchar(500) not null,
+    `refreshtoken` varchar(1024) not null,
+    PRIMARY KEY(`idx`)
+);
+
+//글관련 테이블
+
+create table `side_project_board`(
+    `idx` int(11) not null auto_increment,
+    `user_id` varchar(300) not null,
+    `content` varchar(1024) not null,
+    `image` varchar(1024),
+    `created` datetime not null,
+    PRIMARY KEY(`idx`)
+)
+
+****/
