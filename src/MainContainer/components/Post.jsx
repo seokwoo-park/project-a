fde51c63919322e1cd2ff0334f26fc476254104e
Kooding <giyun529@hanmail.net>
2021-01-16 16:31:28 +0900

    add default image and modify css

--- src/MainContainer/components/Post.jsx
+++ src/MainContainer/components/Post.jsx
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Post.css";
+import defaultImage from "../images/defaultImage.png";
+import defaultProfile from "../images/defaultProfile.png";
 
-function Post({ id, src, title, content, date, tag }) {
-  console.log("[post]: ", title, content, date, tag, src);
+function Post({
+  image = defaultImage,
+  profile = defaultProfile,
+  title,
+  content,
+  tag,
+  date,
+}) {
+  //null check
+  const ifPostImgisNull = (props) =>
+    props === null ? (props = defaultImage) : props;
+  const ifProfileisNull = (props) =>
+    props === null ? (props = defaultProfile) : props;
+
+  const [imgsrc, setImgsrc] = useState({
+    postImage: ifPostImgisNull(image),
+    profileImage: ifProfileisNull(profile),
+  });
+
+  const { postImage, profileImage } = imgsrc;
+
+  //404 error
+  const onError = (e) => {
+    console.log("경로 에러!!");
+    const { name } = e.target;
+    setImgsrc({
+      ...imgsrc,
+      [name]: defaultImage,
+    });
+  };
+
   return (
     <div className="post-container">
-      <div className="post-img"></div>
+      <img
+        className="post-img"
+        name="postImage"
+        src={postImage}
+        alt=""
+        onError={onError}
+      />
       <div className="post-wrap">
-        <div className="profile-img"></div>
+        <img
+          className="profile-img"
+          name="profileImage"
+          src={profileImage}
+          alt=""
+          onError={onError}
+        />
         <h2 className="post-title">{title}</h2>
         <p className="post-content">{content}</p>
-        <div className="post-tags">{tag}</div>
-        <div className="post-date">{date}</div>
+        <div className="post-tags">
+          {tag.map((item) => (
+            <span className="tag-item">#{item} </span>
+          ))}
+        </div>
+        <div className="post-date">
+          {date.slice(0, 10).replaceAll("-", ". ")}
+        </div>
       </div>
     </div>
   );
