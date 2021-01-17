0269e9508afc5e44adc3c8f41bf22bb7d8c652e2
Kooding <giyun529@hanmail.net>
2021-01-17 17:53:19 +0900

    an intermediate save

--- src/MainContainer/components/UserInfo.jsx
+++ src/MainContainer/components/UserInfo.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import defaultUserInfo from "../images/defaultUserInfo.png";
+import "../css/UserInfo.css";
 
 function UserInfo() {
-  return <div className="user-info">MyComponent</div>;
+  return (
+    <div className="user-info">
+      <div className="user-detail">
+        <img src={defaultUserInfo} alt="profile image" />
+        <div className="nickname">koo님</div>
+        <span className="logout-icon"></span>
+      </div>
+      <div className="icons-container">
+        <img src="/images/icons/list-outline.svg" className="list-icon"></img>
+        <img
+          src="/images/icons/list-outline.svg"
+          className="bookmark-icon"
+        ></img>
+        <span className="heart-icon"></span>
+        <span className="edit-introduce-icon"></span>
+        <span className="edit-profile-icon"></span>
+      </div>
+      <span className="introduce-myself">
+        나를 한줄로 표현하자면 어떻게 표현할것인가요? 여기에는 그런 글귀가
+        들어가는 자리 랍니다.
+      </span>
+    </div>
+  );
 }
 
 export default UserInfo;
