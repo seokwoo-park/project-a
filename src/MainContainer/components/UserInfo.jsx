9c450eebf6d301c76225e8e02a44b9ef20f0eaab
Kooding <giyun529@hanmail.net>
2021-01-18 03:53:48 +0900

    use react-icons , userinfo style done

--- src/MainContainer/components/UserInfo.jsx
+++ src/MainContainer/components/UserInfo.jsx
@@ -1,31 +1,54 @@
-import React from "react";
-import defaultUserInfo from "../images/defaultUserInfo.png";
+import React, { useState, useRef } from "react";
+import { useCookies } from "react-cookie";
+import defaultUserImage from "../images/defaultUserInfo.svg";
+import {
+  RiLogoutBoxRLine as LogoutIcon,
+  RiDraftLine as ListIcon,
+  RiFileMarkLine as BookmarkIcon,
+  RiHeartLine as HeartIcon,
+  RiEdit2Line as EditIcon,
+  RiUserSettingsLine as SettingIcon,
+} from "react-icons/ri";
 import "../css/UserInfo.css";
 
 function UserInfo() {
+  const [myself, setMyself] = useState(
+    "나를 한줄로 표현하자면 어떻게 표현할것인가요? 여기에는 그런 글귀가 들어가는 자리"
+  );
+  const [, , removeCookie] = useCookies(["x_auth"]);
+  const myselfEl = useRef();
+  const onClickHandler = () => {
+    removeCookie("x_auth");
+  };
+
+  const onChange = (e) => {
+    setMyself(e.target.value);
+  };
+
   return (
     <div className="user-info">
+      <img className="userImage" src={defaultUserImage} alt="profile image" />
       <div className="user-detail">
-        <img src={defaultUserInfo} alt="profile image" />
         <div className="nickname">koo님</div>
-        <span className="logout-icon"></span>
+        <LogoutIcon className="logout-icon" onClick={onClickHandler} />
       </div>
-      <div className="icons-container">
-        <img src="/images/icons/list-outline.svg" className="list-icon"></img>
-        <img
-          src="/images/icons/list-outline.svg"
-          className="bookmark-icon"
-        ></img>
-        <span className="heart-icon"></span>
-        <span className="edit-introduce-icon"></span>
-        <span className="edit-profile-icon"></span>
+      <div className="icon-box">
+        <ListIcon className="list-icon icon" />
+        <BookmarkIcon className="bookmark-icon icon" />
+        <HeartIcon className="heart-icon icon" />
+        <EditIcon className="edit-icon icon" />
+        <SettingIcon className="setting-icon icon" />
       </div>
-      <span className="introduce-myself">
-        나를 한줄로 표현하자면 어떻게 표현할것인가요? 여기에는 그런 글귀가
-        들어가는 자리 랍니다.
-      </span>
+      <textarea
+        className="introduce-myself"
+        ref={myselfEl}
+        type="text"
+        onChange={onChange}
+        value={myself}
+        placeholder="당신을 소개해 주세요"
+      />
     </div>
   );
 }
 
-export default UserInfo;
+export default React.memo(UserInfo);
