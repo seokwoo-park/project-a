3c1b54c715900f4f5cd0dced6b630f0bb517639f
Kooding <giyun529@hanmail.net>
2021-02-03 16:37:09 +0900

    ui done

--- src/RoutesContainer/components/MyPage.jsx
+++ src/RoutesContainer/components/MyPage.jsx
@@ -1,49 +1,44 @@
-import React, { useEffect } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import { Cookies } from "react-cookie";
-import {getMyPosts} from '../../redux/postRedux/postAction';
-import Post from '../../MainContainer/components/Post';
-import AppBar from '../../MainContainer/components/AppBar';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import MyInfo from "../../MainContainer/components/MyInfo";
+import { getMyPosts } from "../../redux/postRedux/postAction";
+import Post from "../../MainContainer/components/Post";
+import AppBar from "../../MainContainer/components/AppBar";
 
+function MyPage() {
+  const user = useSelector((state) => state.user);
+  const posts = useSelector((state) => state.posts.postList);
+  const dispatch = useDispatch();
+  console.log(posts);
 
-function MyPage() {   
-    
-    const user = useSelector(state => state.user);
-    const posts = useSelector(state => state.posts.postList)
-    const dispatch = useDispatch()
-    console.log(posts)
-    
+  useEffect(() => {
+    dispatch(getMyPosts());
+  }, []);
 
-
-    useEffect(()=>{
-        dispatch(getMyPosts())
-    },[])
-
-
-    return (
-        <div>
-            <AppBar/>
-            <h1>MY INFOMATION</h1>
-            <section className="content-section">
-            {posts.map((post) => {
-            return (
-                <Post
-                className = "content-section"
-                key={post.idx}
-                idx={post.idx}
-                id={post.user_id}
-                title={post.user_id}
-                content={post.content}
-                image={post.image}
-                tag={["react", "javascript", "node"]}
-                date={post.created}
-                profile={post.profile}
-                />
-            );
-            })}
-            </section>
-        </div>
-    )
+  return (
+    <div className="my-page">
+      <AppBar />
+      <MyInfo user={user} />
+      <section className="content-section">
+        {posts.map((post) => {
+          return (
+            <Post
+              className="content-section"
+              key={post.idx}
+              idx={post.idx}
+              id={post.user_id}
+              title={post.user_id}
+              content={post.content}
+              image={post.image}
+              tag={["react", "javascript", "node"]}
+              date={post.created}
+              profile={post.profile}
+            />
+          );
+        })}
+      </section>
+    </div>
+  );
 }
 
-export default MyPage
+export default MyPage;
